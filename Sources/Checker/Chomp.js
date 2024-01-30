9a89f603793d1abf024a53b8de1d845a10cd5274
SoucupB <bogdan.soucup@gmail.com>
2024-01-30 17:35:10 +0200

    Add isValid for expression

--- Sources/Checker/Chomp.js
+++ Sources/Checker/Chomp.js
@@ -1,13 +1,19 @@
 class Chomp {
-  constructor(buffer, index, type, invalid = false) {
+  constructor(buffer, index, type = null, isParent = false, invalid = false) {
     this.buffer = buffer;
     this.index = index;
     this.invalid = invalid;
     this.type = type;
+    this.childrenChomps = [];
+    this.isParent = isParent;
   }
 
   static invalid() {
-    return new Chomp(-1, -1, null, true);
+    return new Chomp(-1, -1, null, false, true);
+  }
+
+  pushChild(chomp) {
+    this.childrenChomps.push(chomp);
   }
 
   isInvalid() {
--- Sources/Checker/Expression.js
+++ Sources/Checker/Expression.js
@@ -2,11 +2,46 @@ import Operator from "./Operator.js";
 import Constant from "./Constant.js";
 import Chomp from "./Chomp.js";
 import Character from "./Character.js";
+import Variable from "./Variable.js";
 
 class Expression {
   static isValid(str) {
-    let i = 0;
+    let index = 0;
+    let chompsArray = [];
+    let chomp = Expression.chompDenominator(str, index);
+    if(chomp.isInvalid()) {
+      return false;
+    }
+    chompsArray.push(chomp);
+    index = chomp.index;
     
+    while(index < str.length) {
+      let signChomp = Operator.chomp(str, index);
+      if(signChomp.isInvalid()) {
+        return false;
+      }
+      chompsArray.push(signChomp);
+      index = signChomp.index;
+
+      chomp = Expression.chompDenominator(str, index);
+      if(chomp.isInvalid()) {
+        return false;
+      }
+      chompsArray.push(chomp);
+      index = chomp.index;
+    }
+    return true;
+  }
+
+  static chompDenominator(str, index) {
+    let possibleCharacters = [Constant.chomp, Variable.chomp]
+    for(let i = 0, c = possibleCharacters.length; i < c; i++) {
+      let chomp = possibleCharacters[i](str, index);
+      if(!chomp.isInvalid()) {
+        return chomp;
+      }
+    }
+    return null;
   }
 }
 
