51c0b74d129cc44dd2606fc1df3acd41f6d200de
SoucupB <bogdan.soucup@gmail.com>
2024-02-01 00:07:09 +0200

    Add initialization specs

--- Sources/tests/Initialization.test.js
+++ Sources/tests/Initialization.test.js
@@ -26,3 +26,38 @@ test('Check Initialization checker v5', (t) => {
   t.equal(Initialization.isValid('cata a=b+3-3+4;'), false, 'returns');
   t.end();
 });
+
+test('Check Initialization checker v6', (t) => {
+  t.equal(Initialization.isValid('int a=b+3-3+(32+4&&2)+4;'), true, 'returns');
+  t.end();
+});
+
+test('Check Initialization checker v7', (t) => {
+  t.equal(Initialization.isValid('char a=b+3-3+(32+4&&2)+4;'), true, 'returns');
+  t.end();
+});
+
+test('Check Initialization checker v8', (t) => {
+  t.equal(Initialization.isValid('char _newVar=b+3-3+(32+4&&2)+4;'), true, 'returns');
+  t.end();
+});
+
+test('Check Initialization checker v9', (t) => {
+  t.equal(Initialization.isValid('char 3_newVar=b+3-3+(32+4&&2)+4;'), false, 'returns');
+  t.end();
+});
+
+test('Check Initialization checker v10', (t) => {
+  t.equal(Initialization.isValid('char _newVar=b+3-3+(32+4&&2)-+4;'), false, 'returns');
+  t.end();
+});
+
+test('Check Initialization checker v11', (t) => {
+  t.equal(Initialization.isValid('char _newVar=b+3-3+(32+4&&||2)+4;'), false, 'returns');
+  t.end();
+});
+
+test('Check Initialization checker v12', (t) => {
+  t.equal(Initialization.isValid('char _newVar=b+3-3+(32+4&&(2||3)/2)+4;'), true, 'returns');
+  t.end();
+});
