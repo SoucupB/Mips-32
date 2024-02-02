a9a5802724f85cbff7e8f391504a8ec16780ed7d
SoucupB <bogdan.soucup@gmail.com>
2024-02-02 20:32:05 +0200

    Add method caller

--- Sources/tests/Methods.test.js
+++ Sources/tests/Methods.test.js
@@ -59,3 +59,19 @@ test('Check Methods checker v8', (t) => {
   t.equal(chomp.isInvalid(), true, 'returns');
   t.end();
 });
+
+test('Check Methods checker v9', (t) => {
+  let chomp = Methods.chompDeclaration('void someMethod(int _aaga,int _dafb){int b=5;}dafafagag', 0);
+
+  t.equal(chomp.isInvalid(), false, 'returns');
+  t.equal(chomp.index, 46, 'returns');
+  t.end();
+});
+
+test('Check Methods checker v10', (t) => {
+  let chomp = Methods.chompDeclaration('void someMethod(int _aaga,int _dafb){int b=5;for(int i=0;i<10;i=i+1){if(a==10){b=10;}a=b;}}dafafagag', 0);
+
+  t.equal(chomp.isInvalid(), false, 'returns');
+  t.equal(chomp.index, 91, 'returns');
+  t.end();
+});
