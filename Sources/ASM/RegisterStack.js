ca3ed8fde23915e8718ffe41a87a238d504c74d7
SoucupB <bogdan.soucup@gmail.com>
2024-02-06 20:21:06 +0200

    WIP

--- Sources/ASM/RegisterStack.js
+++ Sources/ASM/RegisterStack.js
@@ -1,3 +1,15 @@
 export class RegisterStack {
-  
+  constructor() {
+    this.stackValues = [];
+    this.offset = {};
+    this.stackOffset = 0;
+  }
+
+  push(variable, size) {
+    this.offset[variable] = {
+      stack_offset: this.stackOffset,
+      size: size
+    };
+    this.stackOffset += size;
+  }
 }
