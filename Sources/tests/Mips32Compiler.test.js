d8fc94514ec8f768a437a45677473a448cd2582a
SoucupB <bogdan.soucup@gmail.com>
2024-03-03 22:47:37 +0200

    WIP

--- Sources/tests/Mips32Compiler.test.js
+++ Sources/tests/Mips32Compiler.test.js
@@ -13,7 +13,48 @@ test('Code compilation v1', (t) => {
   `)
   mipsCompiler.compile();
   mipsCompiler.run();
-  console.log(mipsCompiler.stdoutBuffer())
+  t.equal(mipsCompiler.stdoutBuffer(), '0 2 4 6 8 10 12 14 16 18 ', 'returns true');
+    
+  t.end();
+});
+
+test('Code compilation v2', (t) => {
+  let mipsCompiler = new Mips32Compiler(`
+    int cmmdc(int a, int b) {
+      if(b == 0) {
+        return a;
+      }
+      return cmmdc(b, a % b);
+    }
+
+    void main() {
+      printNumber(cmmdc(12, 16));
+    }
+  `)
+  mipsCompiler.compile();
+  mipsCompiler.run();
+  t.equal(mipsCompiler.stdoutBuffer(), '4', 'returns true');
+    
+  t.end();
+});
+
+test('Code compilation v3', (t) => {
+  let mipsCompiler = new Mips32Compiler(`
+    int cmmdc(int a, int b) {
+      if(b == 0) {
+        return a;
+      }
+      return cmmdc(b, a % b);
+    }
+
+    void main() {
+      printNumber(cmmdc(3556, 8382));
+    }
+  `)
+  mipsCompiler.compile();
+  mipsCompiler.run();
+  console.log(mipsCompiler.mips32Code().toString())
+  t.equal(mipsCompiler.stdoutBuffer(), '254', 'returns true');
     
   t.end();
 });
