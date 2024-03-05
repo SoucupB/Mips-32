5d89773e2f6641678336e8d1d0a234731d0b2bbf
SoucupB <bogdan.soucup@gmail.com>
2024-03-06 01:20:38 +0200

    WIP

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+import { Mips32Compiler } from "../Sources";
+
 const runButton = document.getElementById('runButton');
 const compileButton = document.getElementById('compileButton');
 
 runButton.addEventListener('click', () => {
   const code = document.getElementById('textarea1').value;
-  console.log('Running code:', code);
+  let mipsCompiler = new Mips32Compiler(code, {
+    stdout: 1024 * 512,
+    stackPointer: 1024 * 1024,
+    memorySize: 1024 * 1024 * 4
+  });
+  mipsCompiler.compile();
+  mipsCompiler.run();
+
+  document.getElementById('textarea2').value = mipsCompiler.stdoutBuffer();
 });
 
 compileButton.addEventListener('click', () => {
   const code = document.getElementById('textarea1').value;
   console.log('Compiling code:', code);
+
+  document.getElementById('textarea2').value = document.getElementById('textarea1').value
 });
+
+const compileCode = () => {
+  const code = document.getElementById('textarea1').value;
+  
+}
