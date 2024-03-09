7be6f32ce997a639ef02cbe2c662e1584da648dd
SoucupB <bogdan.soucup@gmail.com>
2024-03-09 14:22:17 +0200

    Add monaco editor

--- index.js
+++ index.js
@@ -18,9 +18,13 @@ runButton.addEventListener('click', () => {
 
 compileButton.addEventListener('click', () => {
   const code = document.getElementById('textarea1').value;
-  console.log('Compiling code:', code);
-
-  document.getElementById('textarea2').value = document.getElementById('textarea1').value
+  let mipsCompiler = new Mips32Compiler(code, {
+    stdout: 1024 * 512,
+    stackPointer: 1024 * 1024,
+    memorySize: 1024 * 1024 * 4
+  });
+  mipsCompiler.compile();
+  document.getElementById('textarea2').value = mipsCompiler.mips32Code().toString(true);
 });
 
 const compileCode = () => {
