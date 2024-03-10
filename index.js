47f43f7c8b2c8a6e4f1e2356bc7b43d2b926dbe1
SoucupB <bogdan.soucup@gmail.com>
2024-03-10 02:04:12 +0200

    Add compiler

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import { Mips32Compiler } from "./Compiler.js";
 const monaco = require('monaco-editor');
 
 const runButton = document.getElementById('runButton');
-const compileButton = document.getElementById('compileButton');
+const compileButtonMips32 = document.getElementById('compileButtonMips32');
+const compileButtonAsm = document.getElementById('compileButtonAsm');
 
 var editor;
 
 runButton.addEventListener('click', () => {
-  // const code = document.getElementById('textarea1').value;
   const code = editor.getValue();
   let mipsCompiler = new Mips32Compiler(code, {
     stdout: 1024 * 512,
@@ -20,8 +20,7 @@ runButton.addEventListener('click', () => {
   document.getElementById('textarea2').value = mipsCompiler.stdoutBuffer();
 });
 
-compileButton.addEventListener('click', () => {
-  // const code = document.getElementById('textarea1').value;
+compileButtonMips32.addEventListener('click', () => {
   const code = editor.getValue();
   let mipsCompiler = new Mips32Compiler(code, {
     stdout: 1024 * 512,
@@ -32,14 +31,20 @@ compileButton.addEventListener('click', () => {
   document.getElementById('textarea2').value = mipsCompiler.mips32Code().toString(true);
 });
 
-const compileCode = () => {
-  const code = document.getElementById('textarea1').value;
-  
-}
+compileButtonAsm.addEventListener('click', () => {
+  const code = editor.getValue();
+  let mipsCompiler = new Mips32Compiler(code, {
+    stdout: 1024 * 512,
+    stackPointer: 1024 * 1024,
+    memorySize: 1024 * 1024 * 4
+  });
+  mipsCompiler.compile();
+  document.getElementById('textarea2').value = mipsCompiler.intermediaryAsm().toString();
+});
 
 const editorSetup = () => {
   editor = monaco.editor.create(document.getElementById('container'), {
-    value: 'void main() {\n\t\n}',
+    value: 'void main() {\n\tprintNumber(10);\n}',
     language: 'c'
   });
 }
